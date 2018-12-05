9f9ad894cde1c3d31429ca71421b611147d69f84
jeroentvb <1001375@hotmail.nl>
2018-12-05 16:43:48 +0100

    Added background, positioning and scrolling horizontally

--- map/static/assets/js/script.js
+++ map/static/assets/js/script.js
@@ -3,6 +3,13 @@
 import styleLines from './helper.js'
 
 function init () {
+  window.addEventListener('wheel', event => {
+    if (event.deltaY !== 0) {
+      window.scroll(window.scrollX + event.deltaY * 5, window.scrollY)
+      event.preventDefault()
+    }
+  })
+
   fetch(`${window.location.href}data`)
     .then(res => res.json())
     .then(data => setup(data))
@@ -17,9 +24,11 @@ function setup (trafficData) {
 
   const map = L.map('map').setView([coordinates.lat, coordinates.long], 13)
 
-  L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-    maxZoom: 16
+  L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.png', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    maxZoom: 16,
+    minZoom: 2
   })
     .addTo(map)
 
